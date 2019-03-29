78317e06dfe75a2c9169776cdd7d8369f0f83c26
Dmitriy <d.vishnevsky@fast-report.com>
2019-03-29 13:10:59 +0300

    fix(confirm_job): Переключение регистрации в статус потерян при неудачном соединении

--- app/jobs/service_confirm_job.rb
+++ app/jobs/service_confirm_job.rb
@@ -6,12 +6,15 @@ class ServiceConfirmJob < ApplicationJob
 
   def perform(service_registration)
 
-
     uri = URI.parse(service_registration.gate_host)
     params = {method: 'confirm', hashed_token: Digest::MD5.hexdigest(service_registration.token)}
-    response = Net::HTTP.post_form(uri, params)
+    begin 
+      response = Net::HTTP.post_form(uri, params)
+    rescue Errno::ECONNREFUSED => e
+      connection_error = true
+    end
 
-    if response.code == '200' && JSON.parse(response.body)['token'] == service_registration.token
+    if !connection_error && response.code == '200' && JSON.parse(response.body)['token'] == service_registration.token
       service_registration.status = ServiceRegistration.statuses[:confirmed]
       service_registration.save
       ServiceManager.call_updating_job
